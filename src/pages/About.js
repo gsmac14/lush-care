0f846cfb438978dc2595d090a2c3f3a4af845727
Girverius <gsmac1224@gmail.com>
2021-05-06 08:22:18 -0500

    edited style page and about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,9 @@ import NavConsult from '../components/NavConsult';
 import { Button, Navbar, Nav, } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import MedElderly from '../assest/images/MedElderly.jpg'
-
+import Founder from '../assest/images/Founder.png';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
 
 function About() {
@@ -51,14 +53,34 @@ onHide={() => setModalShow(false)}
           </div>
           <div className="col-md-8">
             <div className="card-body" >
-              <h5 className="card-title" style={{textAlign: "center", fontSize: 50, paddingTop: 50}} >Our Care Approach</h5>
-              <p className="card-text" style={{textAlign: 'center', paddingTop: 50, fontSize: 25, paddingRight: 50, paddingLeft: 50}}>Lush Care takes a unique approach with caring for our seniors. We use flexibility and concierge-level services to manage and care for each every individual. This level of care is provided by our innovative, commpassionate, caring and commited team. </p>
+              <h5 className="card-title" style={{textAlign: "center", fontSize: 50, paddingTop: 50}} >Care 24/7</h5>
+              <p className="card-text" style={{textAlign: 'center', paddingTop: 50, fontSize: 25, paddingRight: 50, paddingLeft: 50}}>We are available 24/7 for clients needs including holidays, evenings and weekends</p>
               <p className="card-text"><small className="text-muted"></small></p>
             </div>
             </div>
         
         </div>
       </div>
+
+
+
+      <Jumbotron fluid>
+  <Container>
+    {/* <h1>Fluid jumbotron</h1> */}
+    <h1 style={{textAlign: 'center', fontSize: 30, paddingLeft:300 , paddingRight: 300 }}>
+      Are You Ready To Get Started? 
+    </h1>
+    <p style={{textAlign: 'center'}}>Lush Care can help you or a loved one in need today! Contact now for a complimentray needs assessment.</p>
+    <div style={{textAlign: 'center'}}>
+
+    <Button variant="warning" size="lg" active style={{height: 50, fontSize:15}}>
+    214-912-7372
+  </Button>{' '}
+    </div>
+  </Container>
+</Jumbotron>
+      
+    )
 
       </>
     );
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Media from 'react-bootstrap/Media';
+import Hands_icon from '../assest/images/Hands_icon.png';
+import Friends from '../assest/images/Friends.png';
+import Home_icon from '../assest/images/Home_icon.png';
+import Hospital_icon from '../assest/images/Hospital_icon.png';
+import Hand_Pink from '../assest/images/Hand_Pink.png';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import GearHead from '../assest/images/GearHead.png';
+import { Button} from 'react-bootstrap';
+
 
 function Services () {
 return(
-    <ul className="list-unstyled">
+  <>
+    {/* <ul className="list-unstyled">
   <Media as="li">
     <img
-      width={64}
-      height={64}
+      src={Hands_icon}
+      width={100}
+      height={100}
       className="mr-3"
-      src="holder.js/64x64"
+      alt="Generic placeholder"
+    />
+    <Media.Body>
+      <h5>Home Support</h5>
+      <p>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+        fringilla. Donec lacinia congue felis in faucibus.
+      </p>
+    </Media.Body>
+  </Media>
+
+  <Media as="li">
+    <img
+      width={100}
+      height={100}
+      className="mr-3"
+      src={Friends}
       alt="Generic placeholder"
     />
     <Media.Body>
@@ -26,10 +55,10 @@ return(
 
   <Media as="li">
     <img
-      width={64}
-      height={64}
+      width={100}
+      height={100}
       className="mr-3"
-      src="holder.js/64x64"
+      src={Home_icon}
       alt="Generic placeholder"
     />
     <Media.Body>
@@ -42,26 +71,92 @@ return(
       </p>
     </Media.Body>
   </Media>
+</ul> */}
 
-  <Media as="li">
-    <img
-      width={64}
-      height={64}
-      className="mr-3"
-      src="holder.js/64x64"
-      alt="Generic placeholder"
-    />
-    <Media.Body>
-      <h5>List-based media object</h5>
-      <p>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-        fringilla. Donec lacinia congue felis in faucibus.
-      </p>
-    </Media.Body>
-  </Media>
-</ul>
+<CardDeck style={{paddingTop: 100}}>
+  <Card style={{width: 300, height: 300, border: 'none'}}>
+    <Card.Img variant="top" src={Hands_icon} style={{width: 100, height: 100, marginLeft:160}}/>
+    <Card.Body>
+      <Card.Title style={{textAlign: 'center', marginLeft: 40}}>Personal Care</Card.Title>
+      <Card.Text>
+        <ul style={{textAlign: 'center', listStyle: 'none'}}>
+          <li>Bathing, dressing and grooming</li>
+          <li>Meal Preparation</li>
+          <li>Medication Assistance</li>
+          <li>Exercise Assistance</li>
+        </ul>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <Card style={{width: 300, height: 300, border: 'none'}}>
+    <Card.Img variant="top" src={Friends} style={{width: 100, height: 100, marginLeft:150}} />
+    <Card.Body>
+      <Card.Title style={{textAlign: 'center', marginLeft: 40}}>Companionship</Card.Title>
+      <Card.Text>
+      <ul style={{textAlign: 'center', listStyle: 'none'}}>
+          <li>Shopping and Errands</li>
+          <li>Social and Emotional Support</li>
+          <li>Safety Advisor and Mobility Assistance</li>
+        </ul>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <Card style={{width: 300, height: 300, border: 'none'}} >
+    <Card.Img variant="top" src={Home_icon} style={{width: 100, height: 100, marginLeft:150}} />
+    <Card.Body>
+      <Card.Title style={{textAlign: 'center', marginLeft: 40}}>Home Support</Card.Title>
+      <Card.Text>
+      <ul style={{textAlign: 'center', listStyle: 'none'}}>
+          <li>Houskeeping</li>
+          <li>Laundry</li>
+          <li>Organizing</li>
+        </ul>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+</CardDeck>
+{/*second row*/}
+<CardDeck style={{paddingTop: 50}}>
+  <Card style={{width: 300, height: 300, border: 'none'}}>
+    <Card.Img variant="top" src={GearHead} style={{width: 100, height: 100, marginLeft:160}}/>
+    <Card.Body>
+      <Card.Title style={{textAlign: 'center', marginLeft: 40}}>Memory Care</Card.Title>
+      <Card.Text>
+        <ul style={{textAlign: 'center', listStyle: 'none'}}>
+          <li>Alzheimer's Care</li>
+          <li>Dementia Care</li>
+          <li>Mental Stimulation</li>
+        </ul>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <Card style={{width: 300, height: 300, border: 'none'}}>
+    <Card.Img variant="top" src={Hospital_icon} style={{width: 100, height: 100, marginLeft:150}} />
+    <Card.Body>
+      <Card.Title style={{textAlign: 'center', marginLeft: 40}}>Other Services</Card.Title>
+      <Card.Text>
+      <ul style={{textAlign: 'center', listStyle: 'none'}}>
+          <li>Palliative Support</li>
+          <li>Hospice Support</li>
+          <li>Post-surgery care</li>
+        </ul>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <Card style={{width: 300, height: 300, border: 'none'}} >
+    <Card.Img variant="top" src={Hand_Pink} style={{width: 100, height: 100, marginLeft:150}} />
+    <Card.Body>
+      <Card.Title style={{textAlign: 'center', marginLeft: 40}}>Wait There's More</Card.Title>
+      <Card.Text>
+      <p style={{textAlign: 'center'}}>Lush care is devoted to concierge-level care therefore we tailor our services to each client needs. Additional services can be explored by a simple calls.</p>
+      <Button variant="warning" size="lg" active style={{height: 40, fontSize:15, marginLeft: 150 }}>
+  
+  </Button>{' '}
+      </Card.Text>
+    </Card.Body>
+  </Card>
+</CardDeck>
+</>
 )
 }
 
